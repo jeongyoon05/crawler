4516c40c291b291bb83cf434e8608291264a4026
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-23 14:06:24 +0900

    feat: naver crawler

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { crawl } from "./crawl";
+import { extract } from "./extract";
+
+const app = express();
+app.get("/crawl", async (req, res) => {
+  const result = await crawl();
+  res.send(extract(result).join(", "));
+});
+
+app.listen(8080, () => {
+  console.log("server started!");
+});
